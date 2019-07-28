0c8b27511528a488ebf92ce5d0473450f87c83ac
Mikołaj Malicki <mikolajmalicki000@gmail.com>
2019-07-29 00:14:30 +0200

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,12 +5,12 @@ const forecast = (latitude, longitude, callback) => {
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback('chuja tam, neta nie masz gościu', undefined)
+            callback('Unable to connect with internet', undefined)
         } else if (body.error) {
             callback(`Unable to find location - ${body.error}`, undefined)
         } else {
             const currently = body.currently
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${currently.temperature} degrees out. There is a ${currently.precipProbability}% chance of rain.`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${currently.temperature}°F out. The high today is ${body.daily.data[0].temperatureHigh}°F with a low of ${body.daily.data[0].temperatureLow}°F. There is a ${currently.precipProbability * 100}% chance of rain.`)
         }
     })
 }
